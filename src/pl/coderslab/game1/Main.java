0ade288aa1b18c9693d3a8798f3105ad2b13e5e6
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-04-10 13:42:48 +0200

    scanner działa i sprawdza czy jest to liczba

--- src/pl/coderslab/game1/Main.java
+++ src/pl/coderslab/game1/Main.java
@@ -1,10 +1,25 @@
 package pl.coderslab.game1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Random r = new Random();
+        int maxRandom = 100;
+        int randomNumber = r.nextInt(maxRandom);
 
+//        System.out.println(randomNumber);
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Zgadnij liczbę: ");
+        while (!scan.hasNextInt()) {
+            scan.nextLine();
+            System.out.println("To nie jest liczba, spróbuj ponownie: ");
+        }
+        int number = scan.nextInt();
+//        System.out.println(number);
     }
 }
