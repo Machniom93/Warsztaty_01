5ccb3250bf74ad9f7849b28b4aa8e20adddee660
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-04-11 22:42:59 +0200

    zadanie 3 zrobione

--- src/pl/coderslab/game3/Main.java
+++ src/pl/coderslab/game3/Main.java
@@ -10,21 +10,34 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.println("Pomyśl liczbę od 0 do 1000, a ja ją zgadnę w max. 10 próbach");
 
+        int min = 0;
+        int max = 1000;
+        int counter = 1;
         while (true) {
-            int min = 0;
-            int max = 1000;
-            int counter = 0;
             int guess = ((max-min)/2) + min;
+            String one = "zgadłeś";
             System.out.println("Zgaduję: " + guess);
             System.out.println("Twoja odpowiedź? (za mało/za dużo/zgadłeś): ");
             String answer = scan.nextLine();
-            if (answer == "zgadłeś") {
+            String answer1 = answer.toLowerCase();
+            if (answer1.contains("zgadłeś")) {
                 System.out.println("Wygrałem!");
                 break;
-            } else if (answer == "za dużo") {
-
+            } else if (counter == 10) {
+                System.out.println("Przepraszam nie zgadłem, nie dałem rady;(");
+                break;
+            } else if (answer1.contains("za dużo")) {
+                max = guess;
+                counter++;
+                continue;
+            } else if (answer1.contains("za mało")) {
+                min = guess;
+                counter++;
+                continue;
+            } else {
+                System.out.println("nie oszukuj!");
+                continue;
             }
-
         }
     }
 }
