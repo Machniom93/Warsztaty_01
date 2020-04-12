a17ae303d9377d12040d575da8182987e58164e9
root <root@LAPTOP-CIIBPF5B.localdomain>
2020-04-12 13:59:56 +0200

    pętla znajdująca pierwszy ciąg cyfr

--- src/pl/coderslab/game4/Main.java
+++ src/pl/coderslab/game4/Main.java
@@ -1,4 +1,19 @@
 package pl.coderslab.game4;
 
 public class Main {
+
+    public static void main(String[] args) {
+        String symulator = "555Dy+z";
+        String newText = "";
+        for (int i = 0; i < symulator.length(); i++) {
+            char currentChar = symulator.charAt(i);
+            while (Character.isDigit(symulator.charAt(0)) && Character.isDigit(symulator.charAt(i))) {
+                    newText += currentChar;
+                    i++;
+            }
+
+        }
+        System.out.println(newText);
+
+    }
 }
